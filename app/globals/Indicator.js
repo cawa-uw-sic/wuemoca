249de7fafd45bffa49c3e4bb2473d31e4620c656
rt-inconnect <r.toshpulatov@gmail.com>
2016-12-06 17:48:02 +0100

    for pull

--- app/globals/Indicator.js
+++ app/globals/Indicator.js
@@ -17,8 +17,8 @@ var __Indicator = [
     mapType        : 'labeled',
     enLegend       : ['Irrigation area'],
     ruLegend       : ['Irrigation area'],    
-    median         : 1464,
-    maximum        : 4710,
+    // median         : 1464,
+    // maximum        : 4710,
     chart          : false,
     decimals       : 0,
     glossary       : 'bme00057.htm'
@@ -42,8 +42,8 @@ var __Indicator = [
     mapType        : 'labeled',
     enLegend       : ['All crops', 'Cotton', 'Wheat', 'Rice', 'Other Crop', 'Orchard/Vineyard', 'Urban Garden', 'Maize', 'Vegetable', 'Sunflower'],
     ruLegend       : ['All crops', 'хлопок', 'пшеница', 'рис', 'Other Crop', 'фруктовый сад/Виноградник', 'Urban Garden', 'Maize', 'Vegetable', 'Sunflower'],    
-    median         : [1140, 306, 276, 54, 228, 60, 60, 59, 66, 11],
-    maximum        : [8286, 4230, 4152, 2650, 3756, 3288, 996, 1560, 1548, 469],
+    // median         : [1140, 306, 276, 54, 228, 60, 60, 59, 66, 11],
+    // maximum        : [8286, 4230, 4152, 2650, 3756, 3288, 996, 1560, 1548, 469],
     chart          : ['Stacked', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults'],
     decimals       : 0,
     glossary: 'bme00057.htm'
@@ -66,10 +66,10 @@ var __Indicator = [
     mapType        : 'colored',
     enLegend       : ['All crops', 'Cotton', 'Wheat', 'Rice', 'Other Crop', 'Orchard/Vineyard', 'Urban Garden', 'Maize', 'Vegetable', 'Sunflower'],
     ruLegend       : ['All crops', 'хлопок', 'пшеница', 'рис', 'Other Crop', 'фруктовый сад/Виноградник', 'Urban Garden', 'Maize', 'Vegetable', 'Sunflower'],    
-    median         : [82, 17, 26, 4, 16, 3, 3, 4, 5, 1],
-    maximum        : [145, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+    // median         : [82, 17, 26, 4, 16, 3, 3, 4, 5, 1],
+    // maximum        : [145, 100, 100, 100, 100, 100, 100, 100, 100, 100],
     chart          : ['Stacked', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults', 'Defaults'],
-    legendcolors   : [['#fee0d2','#fc9272','#de2d26'],['#F1F6F2','#76A882','#3B5441'],['#F1F6F2','#FF7F00','#3B5441'],['#F1F6F2','#1F78B4','#3B5441'],['#F1F6F2','#DD3497','#3B5441'],['#F1F6F2','#00858E','#3B5441'],['#F1F6F2','#E31A1C','#3B5441'],['#F1F6F2','#33a02c','#3B5441'],['#F1F6F2','#6a3d9a','#3B5441'],['#F1F6F2','#FFD500','#3B5441']] , 
+    // legendcolors   : [['#fee0d2','#fc9272','#de2d26'],['#F1F6F2','#76A882','#3B5441'],['#F1F6F2','#FF7F00','#3B5441'],['#F1F6F2','#1F78B4','#3B5441'],['#F1F6F2','#DD3497','#3B5441'],['#F1F6F2','#00858E','#3B5441'],['#F1F6F2','#E31A1C','#3B5441'],['#F1F6F2','#33a02c','#3B5441'],['#F1F6F2','#6a3d9a','#3B5441'],['#F1F6F2','#FFD500','#3B5441']] , 
     decimals       : 1,
     glossary: 'bme00057.htm'
   }, {
@@ -89,7 +89,7 @@ var __Indicator = [
     mapType        : 'labeled',
     enLegend       : ['Cotton', 'Wheat'],
     ruLegend       : ['Хлопок', 'Пшеница'],    
-    median         : [[87,270,591,1135,2044,3624,13182], [106,353,761,1421,2511,4371,16691]],
+    // median         : [[87,270,591,1135,2044,3624,13182], [106,353,761,1421,2511,4371,16691]],
     chart          : 'Defaults',
     decimals       : 0,
     glossary: 'bme00057.htm'
@@ -110,9 +110,9 @@ var __Indicator = [
     mapType        : 'colored',
     enLegend       : ['Cotton', 'Wheat'],
     ruLegend       : ['Хлопок', 'Пшеница'],
-    median         : [[1.6, 1.9, 2.2, 2.54, 3.1, 3.7, 25.3], [1.9,2.3,2.7,3.1,3.6,4.1,8.2]],
+    // median         : [[1.6, 1.9, 2.2, 2.54, 3.1, 3.7, 25.3], [1.9,2.3,2.7,3.1,3.6,4.1,8.2]],
     chart          : 'Defaults',
-    legendcolors   : ['#ffff80','#f8db5a','#f0b92f','#e69900','#a38f21','#56823b','#00734c'],
+    // legendcolors   : ['#ffff80','#f8db5a','#f0b92f','#e69900','#a38f21','#56823b','#00734c'],
     decimals       : 1,
     glossary: 'bme00057.htm'
   }, {
@@ -132,11 +132,11 @@ var __Indicator = [
     mapType        : 'colored',
     enLegend       : ['Fallow land'],
     ruLegend       : ['Площадь неисп. земель'],    
-    median         : 28,
-    maximum        : 100,
+    // median         : 28,
+    // maximum        : 100,
     chart          : 'Defaults',
     color          : '#b29972',
-    legendcolors   : ['#F1F6F2','#B29972','#3B5441'],
+    // legendcolors   : ['#F1F6F2','#B29972','#3B5441'],
     decimals       : 1,
     glossary       : 'bme00057.htm'
 }, {
@@ -155,10 +155,10 @@ var __Indicator = [
     mapType        : 'colored',
     enLegend       : ['Crop diversity'],
     ruLegend       : ['Crop diversity'],    
-    median         : 0.7,
-    maximum        : 1,
+    // median         : 0.7,
+    // maximum        : 1,
     chart          : 'Defaults',
-    legendcolors   : ['#d7191c','#ffffbf', '#2c7bb6'],
+    // legendcolors   : ['#d7191c','#ffffbf', '#2c7bb6'],
     color          :'#989800',
     decimals       : 2,
     glossary: 'bme00057.htm'   
@@ -199,10 +199,10 @@ var __Indicator = [
     mapType        : 'colored',
     enLegend       : ['Number of crop types'],
     ruLegend       : ['Количество типов культур'],   
-    median         : 3.4,
-    maximum        : 6,
+    // median         : 3.4,
+    // maximum        : 6,
     chart          : 'Multiannual',
-    legendcolors   : ['#732A06','#e6550d', '#FCEEE6'],
+    // legendcolors   : ['#732A06','#e6550d', '#FCEEE6'],
     glossary       : 'bme00057.htm'
   }, {
     id             : 'frequency',
@@ -220,10 +220,10 @@ var __Indicator = [
     mapType        : 'colored',
     enLegend       : ['Years of fallow'],
     ruLegend       : ['Years of fallow'],    
-    median         : 6,
-    maximum        : 17,
+    // median         : 6,
+    // maximum        : 17,
     chart          : 'Multiannual',
-    legendcolors   : ['#EAF5E9','#33a02c', '#195016'],
+    // legendcolors   : ['#EAF5E9','#33a02c', '#195016'],
     glossary       : 'bme00057.htm'
   }
 
